7858badcf7261995c04e94268faee2ff8536d43a
AnuragAnalog <anurag.peddi1998@gmail.com>
2020-11-02 10:40:45 +0530

    added navie bayes(gaussian)

---
+++ naviebayes.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+class GaussianNB():
+    def __init__(self, priors=None):
+        self.n_classes_ = None
+        if priors is not None:
+            self._check_priors(priors)
+
+    def __str__(self):
+        return "GaussianNB(priors="+str(self.prior_proba_)+")"
+
+    def _check_dtype(self, arr):
+        if not isinstance(arr, np.ndarray):
+            raise ValueError("Expects only numpy ndarrays")
+
+        return
+
+    def _check_priors(self, priors):
+        self._check_dtype(priors)
+
+        if (priors < 0).any():
+            raise ValueError("Probablities cannot be less than 0")
+
+        if (priors > 1).any():
+            raise ValueError("Probablities cannot be greater than 0")
+
+        if int(sum(priors)) != 1:
+            raise ValueError("The sum of the priors should be 1")
+
+        return
+
+    def fit(self, features, labels):
+        self._check_dtype(features)
+        self._check_dtype(labels)
+
+        self.total_samples_ = len(labels)
+        self.n_features_ = features.shape[1]
+        self.n_classes_ = np.unique(labels)
+        self.class_encoding_ = dict()
+        self.class_mean_ = dict()
+        self.class_std_ = dict()
+        self.prior_proba_ = dict()
+
+        for c in self.n_classes_:
+            self.class_mean_[c] = np.mean(features[labels == c], axis=0)
+            self.class_std_[c] = np.std(features[labels == c], axis=0)
+            if self.prior_proba_ is None:            
+                self.prior_proba_[c] = np.sum((labels == c)) / self.total_samples_
+
+        for i, c in enumerate(self.n_classes_):
+            self.class_encoding_[c] = i
+
+        return
+
+    def predict(self, data):
+        self._check_dtype(data)
+
+        if self.n_features_ != data.shape[1]:
+            raise ValueError("Number of features should be", self.n_features_)
+
+        pred = np.zeros((data.shape[0], len(self.n_classes_)))
+        for i, c in enumerate(self.n_classes_):
+            term1 = (1 / (np.sqrt(2 * np.pi * self.class_std_[c]**2)))
+            term2 = np.exp(-((data - self.class_mean_[c])**2 / (2 * self.class_std_[c]**2)))
+            tmp = np.sum(np.log(term1 * term2), axis=1) 
+            pred[:, i] = tmp
+
+        pred = np.argmax(pred, axis=1)
+
+        return pred
+
+    def evaluate(self, X, y, metrics='R2'):
+        self._check_dtype(X)
+        self._check_dtype(y)
+
+        if metrics not in ['R2', 'MSE', 'MAE']:
+            raise ValueError("Possible metrics are [R2, MSE, MAE]")
+
+        pred = self.predict(X)
+
+        if metrics.lower() == 'mse':
+            score = np.mean(np.subtract(y, pred)**2)
+        if metrics.lower() == 'mae':
+            score = np.mean(np.abs(np.subtract(y, pred)))
+        if metrics.lower() == 'r2':
+            score = 1 - ((np.sum(np.sqrt(y, pred))) / (np.sum(np.sqrt(y, np.mean(y)))))
+
+        return score
+
+if __name__ == '__main__':
+    data = pd.read_csv('Iris.csv', index_col='Id')
+
+    train_X, test_X, train_y, test_y = train_test_split(data.loc[:, data.columns != 'Species'], data['Species'], train_size=0.8)
+
+    clf = GaussianNB()
+    clf.fit(train_X.values, train_y.values)
+
+    print(clf.predict(test_X.values))
+    print(clf.evaluate(test_X.values, test_y.map(clf.class_encoding_).values, 'MSE'))
